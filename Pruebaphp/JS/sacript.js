066aee2d90f38c0eefc316cc6ccc336a3052b721
alberto0423 <albertosantes21@hotmail.com>
2018-10-28 17:25:05 -0600

    Subo otra carpeta de las pruebas en php

---
+++ Pruebaphp/JS/sacript.js
@@ -0,0 +1,8 @@
+var suma, sumaUno, sumaDos,result;
+
+sumaUno=parseInt(prompt("introduzca el numero",""));
+sumaDos=parseInt(prompt("introduzca el numero",""));
+result=sumaUno+sumaDos;
+/*suma=alert("Tu resultado es:" + result); */
+
+suma=Document.write("Tu resultado" + result);
---
+++ Pruebaphp/index.php
@@ -0,0 +1,18 @@
+<head>
+    <title>Creacion de un portal con PHP y MySQL</title>
+</head>
+<?php
+$variable_x=10;
+/*
+while($variable_x){
+    echo"<big>";
+    echo"<b>";
+    print("Numero: ".$variable_x);
+    echo"<br>";
+    echo"<hr>";
+}
+*/
+for ($variable_x=5; $variable_x = 10 ; $variable_x++) { 
+    print("Numero: " .$variable_x."<br>");
+}
+?>
